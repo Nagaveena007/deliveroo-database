43a078c5a855795c61d14195a1f492ac63f0dd21
Nagaveena Hallikeri <72273357+Nagaveena007@users.noreply.github.com>
2023-04-10 22:58:07 +0200

    .

--- app.js
+++
@@ -1,6 +0,0 @@
-fetch('https://my-json-server.firebaseio.com/db.json')
-  .then(response => response.json())
-  .then(data => {
-    const jsonContainer = document.getElementById('json-container');
-    jsonContainer.innerHTML = JSON.stringify(data);
-  });
